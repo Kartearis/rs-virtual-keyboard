e1b53cbd4b4c04a61f452f15d919f70d639a5492
Matveev Timofey <justshowel@gmail.com>
2022-05-08 21:49:42 +0300

    feat: Start working on cursor in text area.

--- virtual-keyboard/modules/textarea.js
+++ virtual-keyboard/modules/textarea.js
@@ -13,10 +13,44 @@ export default class Textarea {
         this.textarea = document.createElement('textarea');
         this.textarea.classList.add('textarea');
         this.textarea.readOnly = true;
+        // this.#makeCustomReadonly();
+        this.#addCursor();
         this.element.appendChild(this.textarea);
     }
 
+    #addCursor() {
+        this.textarea.value += '|';
+    }
+
+    #removeCursor() {
+        this.textarea.value = this.textarea.value.slice(0, -1);
+    }
+
+    // #makeCustomReadonly() {
+    //     this.textarea.addEventHandler('keydown', (e) => {
+    //         e.preventDefault();
+    //     });
+    // }
+
     addText(text) {
+        this.#removeCursor();
         this.textarea.value += text;
+        this.#addCursor();
+    }
+
+    backspace() {
+        if (this.textarea.value.length > 0)
+            this.textarea.value = this.textarea.value.slice(0, -1);
+    }
+
+    delete() {
+
+    }
+
+    nav_left() {
+        let currentPosition = this.textarea.selectionStart;
+        console.log(currentPosition);
+        if (currentPosition > 0)
+            this.textarea.setSelectionRange(currentPosition - 1, currentPosition - 1);
     }
 }
