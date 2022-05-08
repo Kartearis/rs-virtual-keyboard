4f4fbdee18df369af203ee7fca84ebae8efcd2a7
Matveev Timofey <justshowel@gmail.com>
2022-05-09 01:05:49 +0300

    feat: Implement fake cursor and navigation

--- virtual-keyboard/modules/textarea.js
+++ virtual-keyboard/modules/textarea.js
@@ -3,6 +3,7 @@
 export default class Textarea {
     element = null;
     textarea = null;
+    currentPosition = 0;
 
     constructor(element) {
         this.element = element;
@@ -13,44 +14,126 @@ export default class Textarea {
         this.textarea = document.createElement('textarea');
         this.textarea.classList.add('textarea');
         this.textarea.readOnly = true;
-        // this.#makeCustomReadonly();
+        this.currentPosition = 0;
         this.#addCursor();
         this.element.appendChild(this.textarea);
     }
 
+    #insertToTextarea(index, value) {
+        this.textarea.value = this.textarea.value.slice(0, index) + value + this.textarea.value.slice(index);
+    }
+
+    #removeFromTextarea(index) {
+        this.textarea.value = this.textarea.value.slice(0, index - 1) + this.textarea.value.slice(index);
+    }
+
     #addCursor() {
-        this.textarea.value += '|';
+        this.#insertToTextarea(this.currentPosition, '╎');
     }
 
     #removeCursor() {
-        this.textarea.value = this.textarea.value.slice(0, -1);
+        this.textarea.value = this.textarea.value.slice(0, this.currentPosition) + this.textarea.value.slice(this.currentPosition+1);
     }
-
-    // #makeCustomReadonly() {
-    //     this.textarea.addEventHandler('keydown', (e) => {
-    //         e.preventDefault();
-    //     });
-    // }
 
     addText(text) {
         this.#removeCursor();
-        this.textarea.value += text;
+        this.#insertToTextarea(this.currentPosition, text);
+        this.currentPosition += text.length;
         this.#addCursor();
     }
 
     backspace() {
-        if (this.textarea.value.length > 0)
-            this.textarea.value = this.textarea.value.slice(0, -1);
+        if (this.currentPosition > 0)
+        {
+            this.#removeCursor();
+            this.#removeFromTextarea(this.currentPosition);
+            this.currentPosition--;
+            this.#addCursor();
+        }
     }
 
     delete() {
-
+        if (this.currentPosition < this.textarea.value.length - 1)
+        {
+            this.#removeCursor();
+            this.#removeFromTextarea(this.currentPosition + 1);
+            this.#addCursor();
+        }
     }
 
     nav_left() {
-        let currentPosition = this.textarea.selectionStart;
-        console.log(currentPosition);
-        if (currentPosition > 0)
-            this.textarea.setSelectionRange(currentPosition - 1, currentPosition - 1);
+        if (this.currentPosition > 0)
+        {
+            this.#removeCursor();
+            this.currentPosition--;
+            this.#addCursor();
+        }
+    }
+
+    nav_right() {
+        if (this.currentPosition < this.textarea.value.length - 1)
+        {
+            this.#removeCursor();
+            this.currentPosition++;
+            this.#addCursor();
+        }
+    }
+    // All row related methods could be optimized by storing cache of row-start positions
+    #getCurrentCol() {
+        let colNum = 0;
+        for (let i = 0; i < this.currentPosition; i++) {
+            if (this.textarea.value[i] === '\n')
+                colNum = 0;
+            else colNum++;
+        }
+        return colNum;
+    }
+
+    #getCurrentRow() {
+        return (this.textarea.value.slice(0, this.currentPosition).match(/\n/g) || []).length;
+    }
+
+    #getNumRows() {
+        return (this.textarea.value.match(/\n/g) || []).length + 1;
+    }
+
+    #calculatePosition(row, column) {
+        let currentRow = 0;
+        let rowPosition = 0;
+        let length = this.textarea.value.length;
+        for (let i = 0; i < length && currentRow < row; i++)
+            if (this.textarea.value[i] === '\n')
+            {
+                currentRow++;
+                rowPosition = i + 1;
+            }
+        let colPosition = rowPosition;
+        for (let i = 0; i < column && rowPosition + i < length; i++)
+            if (this.textarea.value[rowPosition + i] !== '\n') colPosition++;
+            else break;
+        if (colPosition >= length) return length;
+        return colPosition;
+    }
+
+    nav_up() {
+        let row = this.#getCurrentRow();
+        if (row > 0)
+        {
+            this.#removeCursor();
+            let col = this.#getCurrentCol();
+            this.currentPosition = this.#calculatePosition(row - 1, col);
+            this.#addCursor();
+        }
+    }
+
+    nav_down() {
+        let row = this.#getCurrentRow();
+        if (row < this.#getNumRows() - 1)
+        {
+            this.#removeCursor();
+            let col = this.#getCurrentCol();
+            this.currentPosition = this.#calculatePosition(row + 1, col);
+            this.#addCursor();
+        }
     }
 }
