f0dc881c45af65decb8ed469cbbd504d0a823579
Matveev Timofey <justshowel@gmail.com>
2022-05-09 16:29:54 +0300

    feat: Add tip on language switching.
    Also add shadows to keyboard.

--- virtual-keyboard/index.js
+++ virtual-keyboard/index.js
@@ -1,7 +1,9 @@
 import VirtualKeyboard from "./modules/virtual-keyboard.js";
 import Textarea from "./modules/textarea.js";
+import Tip from "./modules/Tip.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     const textarea = new Textarea(document.body);
+    const tip = new Tip(document.body, 'This is Windows qwerty keyboard.<br>To switch language press SHIFT+ALT.');
     const keyBoard = new VirtualKeyboard(document.body, 'load', textarea);
 });
